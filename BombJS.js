e9175504f3e55991f9cbf5aab2515f46ce1da64c
eltgm <heroesofmagic14>
2017-03-29 20:09:49 +0300

    таймер, "проволока", жизни

--- BombJS.js
+++ BombJS.js
@@ -3,6 +3,10 @@
  */
 /*
 код с таймера, проверок*/
+var lifes = 3;
+var minute = 15;
+var second = 0
+
 function end() {
     alert("Boom");
 }
@@ -12,13 +16,40 @@ function timerInit() {
 }
 
 function checkDefuse() {
-    document.getElementById('sound').setAttribute('src',"boom.mp3");
+    var checked = 0;
+    if(lifes == 0) {
+        document.getElementById('sound').setAttribute('src', 'boom.mp3');
+        document.getElementById('sound').setAttribute('loop', '');
+        document.getElementById('init').innerHTML = "timerInit(0)";
+    }else{
+        var rad=document.getElementsByName('preferred_color');
+        for (var i=0;i<rad.length; i++) {
+            if (rad[i].checked) {
+                checked = i;
+            }
+        }
+        if(checked != 0){
+            lifes--;
+            alert("Осталось " + lifes + " жизней")
+        }
+    }
 }
 
-function refresh() {
-    var minute = new Date().getMinutes();
-    var second = new Date().getSeconds();
-
+function refresh() {;
+    if(second == 0){
+        minute--;
+        second = 60;
+    }
+    second--;
     document.getElementById('min').innerHTML = minute;
     document.getElementById('sec').innerHTML = second;
+}
+
+function fun1() {
+    var rad=document.getElementsByName('preferred_color');
+    for (var i=0;i<rad.length; i++) {
+        if (rad[i].checked) {
+            alert('Выбран '+i+' radiobutton');
+        }
+    }
 }
